5c3d8b81cfea1208993294a44611325439db06dd
pedro-alves16 <pedroalves16.dev@gmail.com>
2025-06-02 13:40:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+
+  // 
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+ 
+});
